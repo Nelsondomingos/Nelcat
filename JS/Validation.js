2b8041e0c8b288db6b0526366760106b954f1d08
Nelsondomingos <nelsondomingos412@gmail.com>
2024-03-12 21:40:29 -0300

    :rocket: send project with new version

--- JS/Validation.js
+++ JS/Validation.js
@@ -1,94 +1,109 @@
-let GetInpuElement =  document.querySelector('#password'),
-eyes = document.querySelector('#eyes')
-verification = true
-userInter = document.querySelector('.confirm'),
-errorLog = document.querySelector('#errorLog');
+let GetInpuElement = document.querySelector("#password"),
+  eyes = document.querySelector("#eyes");
+verification = true;
+(userInter = document.querySelector(".confirm")),
+  (errorLog = document.querySelector("#errorLog"));
 
-
-function showPassWord () {
-    if(verification == true){
-        GetInpuElement.setAttribute('type', 'text');
-        document.querySelector('#eyes').style.backgroundImage = "url('../../IMG/hidden.png')";
-        verification = false;
-    } else {
-        verification = true;
-        GetInpuElement.setAttribute('type', 'password');
-        document.querySelector('#eyes').style.backgroundImage = "url('../../IMG/visiblite.png')";
-    }
+function showPassWord() {
+  if (verification == true) {
+    GetInpuElement.setAttribute("type", "text");
+    document.querySelector("#eyes").style.backgroundImage =
+      "url('../../IMG/hidden.png')";
+    verification = false;
+  } else {
+    verification = true;
+    GetInpuElement.setAttribute("type", "password");
+    document.querySelector("#eyes").style.backgroundImage =
+      "url('../../IMG/visiblite.png')";
+  }
 }
 
 function validarFormulario() {
-    // Resetando mensagem de erro
-    document.getElementById('email').innerHTML = '';
-    document.getElementById('password').innerHTML = '';
+  // Resetando mensagem de erro
+  document.getElementById("email").innerHTML = "";
+  document.getElementById("password").innerHTML = "";
 
-    // Pegando os valores dos inputs
-    let input1Value = document.getElementById('email').value.trim();
-    let input2Value = document.getElementById('password').value.trim();
+  // Pegando os valores dos inputs
+  let input1Value = document.getElementById("email").value.trim();
+  let input2Value = document.getElementById("password").value.trim();
 
-    // Validando o Input1 e se foi digitado @gmail.com
-    if (input1Value === '' || !input1Value.includes('@') || !input1Value.includes('.com')) {
-        document.getElementById('email').style.border = '1px solid red';
-        document.getElementById('btn').style.cursor = 'no-drop';
-        return;
-    } else{
-        document.getElementById('email').style.border = '1px solid green';
-        document.getElementById('btn').style.cursor = 'pointer';
-    }
-    // Validando Input 2
-    if (input2Value.trim() === '' || input1Value < 6 ) {
-        document.getElementById('password').style.border="1px solid red";
-        let button = document.getElementById('btn').style.cursor = "no-drop";
-        return;
-    } 
+  // Validando o Input1 e se foi digitado @gmail.com
+  if (
+    input1Value === "" ||
+    !input1Value.includes("@") ||
+    !input1Value.includes(".com")
+  ) {
+    document.getElementById("email").style.border = "1px solid red";
+    document.getElementById("btn").style.cursor = "no-drop";
+    return;
+  } else {
+    document.getElementById("email").style.border = "1px solid green";
+    document.getElementById("btn").style.cursor = "pointer";
+  }
+  // Validando Input 2
+  if (input2Value.trim() === "" || input1Value < 6) {
+    document.getElementById("password").style.border = "1px solid red";
+    let button = (document.getElementById("btn").style.cursor = "no-drop");
+    return;
+  }
 }
 
-let button = document.getElementById('btn').addEventListener('click',validarFormulario);
+let button = document
+  .getElementById("btn")
+  .addEventListener("click", validarFormulario);
 
-// Este é quando os inputs forem bem preenchidos 
-let myForm = document.querySelector('#myForm').addEventListener('submit', function (event){
+// Este é quando os inputs forem bem preenchidos
+let myForm = document
+  .querySelector("#myForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    document.querySelector('#specialSpan').style.display = "none";
-    let carregando = document.querySelector('.carregando').style.display = "block";
+    document.querySelector("#specialSpan").style.display = "none";
+    let carregando = (document.querySelector(".carregando").style.display =
+      "block");
 
-    let input2Value = document.getElementById('password').style.border = "1px solid green";
-    
-    setTimeout (function timeoutFunction (){
-        document.querySelector('#specialSpan').style.display = "block";
-        document.querySelector('.carregando').style.display = "none";
-        
-        function verificarLoginNoLocalStorage(event) {
+    let input2Value = (document.getElementById("password").style.border =
+      "1px solid green");
 
-            let email = document.getElementById('email').value;
-            let password = document.getElementById('password').value;         
+    setTimeout(function timeoutFunction() {
+      document.querySelector("#specialSpan").style.display = "block";
+      document.querySelector(".carregando").style.display = "none";
 
-            // Obter usuários do localStorage
-            let users = JSON.parse(localStorage.getItem('users')) || [];
-        
-            // Verificar se existe um usuário com o mesmo email e senha
-            let usuarioExistente = users.find(user => user.email === email && user.password === password);
-        
-            if (usuarioExistente) {
+      function verificarLoginNoLocalStorage(event) {
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
 
-                // Armazenar dados do usuário atual na sessionStorage para uso posterior
-                sessionStorage.setItem('currentUser', JSON.stringify(usuarioExistente));
-                window.location ="../LOGADO/index.html";
+        // Obter usuários do localStorage
+        let users = JSON.parse(localStorage.getItem("users")) || [];
 
-                localStorage.removeItem('userLog');
+        // Verificar se existe um usuário com o mesmo email e senha
+        let usuarioExistente = users.find(
+          (user) => user.email === email && user.password === password
+        );
 
-                let currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+        if (usuarioExistente) {
+          // Armazenar dados do usuário atual na sessionStorage para uso posterior
+          sessionStorage.setItem(
+            "currentUser",
+            JSON.stringify(usuarioExistente)
+          );
+          window.location = "../LOGADO/index.html";
 
-            } else {
-                errorLog.style.display ="block";
-                document.querySelector('.cadastre').style.marginTop ="-3rem";
-                document.querySelector('.otherWay').style.display ="none";
-                document.getElementById('email').style.border = "1px solid  rgb(238, 230, 230)";
-                document.getElementById('password').style.border = "1px solid  rgb(238, 230, 230)";
-            }
-            event.preventDefault();
+          localStorage.removeItem("userLog");
+
+          let currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
+        } else {
+          errorLog.style.display = "block";
+          document.querySelector(".cadastre").style.marginTop = "-3rem";
+          document.querySelector(".otherWay").style.display = "none";
+          document.getElementById("email").style.border =
+            "1px solid  rgb(238, 230, 230)";
+          document.getElementById("password").style.border =
+            "1px solid  rgb(238, 230, 230)";
         }
-        verificarLoginNoLocalStorage();
-        document.querySelector('#myForm').submit();
-    },2000);
-});
+        event.preventDefault();
+      }
+      verificarLoginNoLocalStorage();
+      document.querySelector("#myForm").submit();
+    }, 2000);
+  });
--- JS/consult.js
+++ JS/consult.js
@@ -1,45 +1,52 @@
-let informationConsult = JSON.parse (localStorage.getItem('agendada'));
-let toast = document.querySelector('.toast');
+let informationConsult = JSON.parse(localStorage.getItem("agendada"));
+let toast = document.querySelector(".toast");
 
-if(informationConsult == '' || informationConsult == null || informationConsult == undefined){
-    toast.style.display ="block";
-    
-    setTimeout (function close (){
-        window.location ="index.html";
-    }, 5000);
+if (
+  informationConsult == "" ||
+  informationConsult == null ||
+  informationConsult == undefined
+) {
+  toast.style.display = "block";
+
+  setTimeout(function close() {
+    window.location = "index.html";
+  }, 5000);
 } else {
-    toast.style.display ="none";
-    let name = document.querySelector('#name').innerHTML = informationConsult[0].name;
-    email = document.querySelector('#email').innerHTML = informationConsult[0].email;
-    typeConsult = document.querySelector('#consulta').innerHTML = informationConsult[0].type;
-    date = document.querySelector('#date').innerHTML = informationConsult[0].date;
-    loadin = document.querySelector('.carregando');
-    btn = document.querySelector('.btn');
-    container = document.querySelector('.container');
-    closeElement = document.querySelector('#close').addEventListener('click', function (){
-        window.location ="index.html";
+  toast.style.display = "none";
+  let name = (document.querySelector("#name").innerHTML =
+    informationConsult[0].name);
+  email = document.querySelector("#email").innerHTML =
+    informationConsult[0].email;
+  typeConsult = document.querySelector("#consulta").innerHTML =
+    informationConsult[0].type;
+  date = document.querySelector("#date").innerHTML = informationConsult[0].date;
+  loadin = document.querySelector(".carregando");
+  btn = document.querySelector(".btn");
+  container = document.querySelector(".container");
+  closeElement = document
+    .querySelector("#close")
+    .addEventListener("click", function () {
+      window.location = "index.html";
     });
 
-    btn.addEventListener('click', function (){
-        var promptMessage = prompt('Deseja cancelar a consulta ?');
+  btn.addEventListener("click", function () {
+    var promptMessage = prompt("Deseja cancelar a consulta ?");
 
-        if(promptMessage == 'Sim' || promptMessage == 'sim'){
-            container.style.display ="none";
-            loadin.style.display ="block";
+    if (promptMessage == "Sim" || promptMessage == "sim") {
+      container.style.display = "none";
+      loadin.style.display = "block";
 
-            setTimeout (function (){
-                window.location ="index.html"
-                informationConsult = localStorage.removeItem('agendada');
-            }, 500);
-        } 
-        else if (promptMessage == 'Não' || promptMessage == 'não'){
-                alert('não');
-            } 
-            else{
-            alert('Tens que digita ou Não ou Sim');
-        }
-    });
+      setTimeout(function () {
+        window.location = "index.html";
+        informationConsult = localStorage.removeItem("agendada");
+      }, 500);
+    } else if (promptMessage == "Não" || promptMessage == "não") {
+      alert("não");
+    } else {
+      alert("Tens que digita ou Não ou Sim");
+    }
+  });
 
-    container.style.display ="block"
-    date.style.display ="none";
+  container.style.display = "block";
+  date.style.display = "none";
 }
